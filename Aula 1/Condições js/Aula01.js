6be75fc7dd8381727f6130f5c1bcf3064aaec308
Willian Choy Cheng Martins <willianchoy@yahoo.com.br>
2023-07-21 11:18:01 -0300

    javascript

---
+++ Aula 1/Condições js/Aula01.js
@@ -0,0 +1,7 @@
+var vel = 61;
+console.log(`A velocidade do seu carro é ${vel}Km por hora`);
+if (vel > 60) {
+  //condição simples, pois não tem else (senão)
+  console.log('Você ultrapassou a velocidade permitida! Multado!');
+}
+console.log('Dirija sempre usando o cinto de segurança');
